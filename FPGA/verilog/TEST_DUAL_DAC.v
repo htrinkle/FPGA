142f3653bd9f3502b465347340d44d178c3fd458
htrinkle <htrinkle@telstra.com>
2025-03-06 15:18:59 +1100

    Update TEST_DUAL_DAC.v

--- FPGA/verilog/TEST_DUAL_DAC.v
+++ FPGA/verilog/TEST_DUAL_DAC.v
@@ -13,6 +13,7 @@ localparam LED_CTR_WIDTH = $clog2(24_000_000 / 2);
 reg [7:0] buf1, buf0;
 wire [7:0] ramp;
 wire [LED_CTR_WIDTH-1:0] led_q;
+wire pll_clk;
 
 // IO Assignments
 assign dac_a_c = clk;
@@ -21,14 +22,18 @@ assign dac_a_d = buf1;
 assign dac_b_d = buf1;
 assign led = led_q[LED_CTR_WIDTH-1 : LED_CTR_WIDTH-3];
 
+// PLL
+PLL pll_inst(.inclk0(clk), .c0(pll_clk));
+// assign pll_clk = clk;  // bypass pl
+
 // Ramp Counter 
-COUNTER_SR #(.BITS(8)) ramp_ctr(.clk(clk), .sReset(~button), .q(ramp));
+COUNTER_SR #(.BITS(8)) ramp_ctr(.clk(pll_clk), .sReset(~button), .q(ramp));
 
 // LED Counter
 COUNTER_SR #(.BITS(LED_CTR_WIDTH)) led_ctr(.clk(clk), .sReset(~button), .q(led_q));
 
 // Neg-edge triggered buffer for DAC data output
-always @(negedge clk)
+always @(negedge pll_clk)
 begin
     {buf1, buf0} = {~buf0, ramp};
 end
